01c056f54dfca40972a8f5adc2a8e9d58854271c
Genius Chinembiri <geniuschinembiri@gmail.com>
2024-01-26 23:27:00 +0200

    feat: add the size prop to the Text component

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Text as ReactNativeText } from 'react-native';
 import type { TextProps as ReactNativeTextProps } from 'react-native';
 import { useTheme } from '../hooks/useTheme';
+import type { FontSize } from '../constants/font';
 
 export interface TextProps extends ReactNativeTextProps {
+  size?: keyof typeof FontSize;
   color?: string;
   darkColor?: string;
 }
 
 export function Text(props: TextProps) {
-  const { color, darkColor, style, ...restOfProps } = props;
+  const { color, darkColor, style, size, ...restOfProps } = props;
   const { isDarkMode } = useTheme();
 
   const theme = useTheme();
@@ -18,6 +20,7 @@ export function Text(props: TextProps) {
     <ReactNativeText
       style={{
         color: isDarkMode ? darkColor || theme.color.Gray[300] : color,
+        fontSize: theme.fontSize[size || 'base'],
         ...(style as any),
       }}
       {...restOfProps}
